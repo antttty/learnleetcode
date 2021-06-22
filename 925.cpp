b63ffd37529a641d97a91100e2e0b4340fbcb150
1464683976 <1464683976@qq.com>
2021-06-22 12:17:49 +0800

    修改islongpressedname

--- 925.cpp
+++ 925.cpp
@@ -1,6 +1,6 @@
 #include <string>
 using namespace std;
-
+/*修改一个文件后 再提交到暂存区 版本库*/
 bool isLongPressedName(string name, string typed) {
         int index1 = 0, index2 = 0;
         while(index1 < name.length() || index2 < typed.length()){
